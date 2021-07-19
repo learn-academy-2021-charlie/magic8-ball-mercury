3493d89c9f3907eee12fd1b956eb66e0a5984920
ArtyG-10 <a.gourent@outlook.com>
2021-07-19 18:59:34 -0400

    switch driver

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     super(props)
     this.state = {
       // ACTION ITEM: Add possible Magic8 Ball answers to the array
-      answerArray: ["Here is an answer"],
+      answerArray: ["Here is an answer: No", "Here is an answer: Yes", "Here is an answer: Do it later", "Here is an answer: Ask again later" ],
       selectedAnswer: ""
     }
   }
@@ -14,8 +14,20 @@ class App extends Component {
   handleSubmit = () => {
     const { answerArray } = this.state
     // ACTION ITEM: Your code here!!
+    let randomNum = Math.floor(Math.random() * 4);
     // ACTION ITEM: Update the answerArray selection
-    this.setState({ selectedAnswer: answerArray[0] })
+    if (randomNum === 0){
+      this.setState({ selectedAnswer: answerArray[0] })
+    }
+    else if (randomNum === 1){
+      this.setState({ selectedAnswer: answerArray[1] })
+    }
+    else if (randomNum === 2){
+      this.setState({ selectedAnswer: answerArray[2] })
+    }
+    else if (randomNum === 3){
+      this.setState({ selectedAnswer: answerArray[3] })
+    }
   }
 
   render(){
@@ -28,7 +40,7 @@ class App extends Component {
         />
         <br />
         <button onClick={ this.handleSubmit }>
-          Ask the Magic 8 Ball a Question
+          Ask the Magic 8 Ball a Question!
         </button>
         <p>{ this.state.selectedAnswer }</p>
       </>
